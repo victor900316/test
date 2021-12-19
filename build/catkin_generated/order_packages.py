384a6dbc99e3b654651936e77c03f2d46e282a0c
victor <chengyundeng123@gmail.com>
2021-12-19 13:59:03 +0800

    lesson 2 add C_one

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/victor/test/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/victor/test/devel;/opt/ros/melodic'.split(';') if '/home/victor/test/devel;/opt/ros/melodic' != '' else []
